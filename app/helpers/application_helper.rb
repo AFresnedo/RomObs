ea76d2af084755467907dde7eac2c16f5147452e
AFresnedo <AFresnedo@users.noreply.github.com>
2016-09-26 17:40:31 -0700

    style footer and header partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# Returns the full title page on a per-page basis
+  def full_title(page_title = '')
+    base_title = "Roman's Observatory"
+    if page_title.empty?
+      base_title #this is an implicit return, courtesy of ruby
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
