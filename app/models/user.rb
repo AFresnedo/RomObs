3266d3bd3229a46d37d16b1dadbc04c93ec287db
AFresnedo <AFresnedo@users.noreply.github.com>
2016-09-28 11:52:28 -0700

    fix db errors, augment seeds with activation tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
-
-  attr_accessor :remember_token
-
-  before_save { email.downcase! } #this syntax works because in User class
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_token
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password # doesn't prevent blank password
-  # presence: true prevents blank password
+  has_secure_password
+  # presence: true prevents blank password, has_secure_password doesn't
   # allow_nil allows empty password updates...meh feature
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
@@ -28,14 +27,23 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-end
 
-def User.digest(string)
-  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-    BCrypt::Engine.cost
-  BCrypt::Password.create(string, cost: cost)
-end
+  # class methods, as opposed to instance methods
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-def User.new_token
-  SecureRandom.urlsafe_base64
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def create_activation_token
+    self.activation_token = User.new_token
+    # update_attribute isn't necessary because not in db yet (before_create)
+    self.activation_digest = User.digest(activation_token)
+  end
 end
