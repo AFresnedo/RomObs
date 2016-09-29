183cebc635edfa32a5f447993435f35aa5ce65cb
AFresnedo <AFresnedo@users.noreply.github.com>
2016-09-29 15:15:25 -0700

    add account activation validations to users index and show, reduce account activations controller db hits

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,8 +3,7 @@ class AccountActivationsController < ApplicationController
     user = User.find_by_email(params[:email])
     # pre: params[:id] is the token value assigned to user
     if user && !user.activated && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated, true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      user.update_columns(activated: true, activated_at: Time.zone.now)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
