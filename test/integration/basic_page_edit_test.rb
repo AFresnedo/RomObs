62f3d94b7b8ec2578e8038dd093952add683d4a6
AFresnedo <AFresnedo@users.noreply.github.com>
2016-10-14 15:18:34 -0700

    begin basic page edit integration test

---
+++ test/integration/basic_page_edit_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+class BasicPageEditTest < ActionDispatch::IntegrationTest
+  # this integration test ensures that all the edit features are displayed on
+  # the basic pages when/only when an admin is logged in and browsing them
+end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @aboutArticle = articles(:aboutPageArticleOne)
+    @new_contact = Article.new(title: "title", body: "body", page: 'contact')
+  end
+
+  test "valid contact article is saved" do
+    article = @new_contact
+    # easier to do article.valid? but here for learning references
+    assert_difference 'Article.count', 1 do
+      article.save
+    end
+  end
+
+  test "about page is valid" do
+    @new_contact.page = 'whoops'
+    assert_not @new_contact.valid?
+    @new_contact.page = 'about'
+    assert @new_contact.valid?
+  end
+
+  test "invalid article is not saved" do
+    article = Article.new
+    assert_not article.valid?
+    # since article is invalid, checking difference is redundant
+    assert_difference 'Article.count', 0 do
+      article.save
+    end
+  end
+
+  test "article with an invalid page is invalid" do
+    article = @new_contact
+    article.page = 'wrong'
+    assert_not article.valid?
+  end
+
+  test "blank title is invalid" do
+    article = @new_contact
+    assert article.valid?
+    article.title = " " * 10
+    assert_not article.valid?
+  end
+
+  test "blank body is invalid" do
+    article = @new_contact
+    assert article.valid?
+    article.body = " " * 100
+    assert_not article.valid?
+  end
+
 end
